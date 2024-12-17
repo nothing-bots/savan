414d0488db16309d42196c4e9bfe2f4b61ad79a9
Tosu <terebina860@gmail.com>
2024-12-18 02:48:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH", "")
 
-botusername = int(getenv("BOT_USERNAME"))
+botusername = int(getenv("BOT_USERNAME", "@anya_x_damian_bot"))
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6848223695").split()))
